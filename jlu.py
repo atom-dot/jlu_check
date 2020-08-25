c98d0cfec29de0052bd04d44d2e65d4eaae6fc4d
Leon <1578644088@qq.com>
2020-08-25 17:57:05 +0800

    bug fixed

--- jlu.py
+++ jlu.py
@@ -80,7 +80,7 @@ def jlu_check(username, password, major, grade, campus, apartment, bedroom, mast
         # 体温状态
         if time.localtime().tm_hour != 21:
             css_code = {
-                0 : "table.xdLayout > tbody > tr:nth-last-child(4) > td:nth-child(3) > div > div > input:nth-child(1)",
+                0 : "table.xdLayout > tbody > tr:nth-last-child(8) > td:nth-child(3) > div > div > input:nth-child(1)",
                 1 : "table.xdLayout > tbody > tr:nth-last-child(6) > td:nth-child(3) > div > input:nth-child(1)",
                 2 : "table.xdLayout > tbody > tr:nth-last-child(4) > td:nth-child(3) > div > div > input:nth-child(1)"
             }.get(nth)
